85ba485127c05c3772a6811ae5b10bff7ff8a5d9
rutujajadhav00 <rutujajadhav413512@gmail.com>
2025-05-07 21:24:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+namespace FactorialApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to calculate factorial: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            long factorial = CalculateFactorial(number);
+
+            Console.WriteLine("Factorial of " + number + " is: " + factorial);
+            Console.ReadLine(); // Keep the console open
+        }
+
+        static long CalculateFactorial(int n)
+        {
+            if (n < 0)
+            {
+                Console.WriteLine("Factorial is not defined for negative numbers.");
+                return -1;
+            }
+
+            long result = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
